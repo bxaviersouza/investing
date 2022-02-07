1f2c291f016f5f4ca21dd2118ed264ae2171b5d9
Bruno <b.xavier.souza@gmail.com>
2022-02-07 13:32:48 -0300

    Projeto Criado

---
+++ src/test/java/com/bbruno/investing/InvestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbruno.investing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
